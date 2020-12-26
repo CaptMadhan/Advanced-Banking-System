f79574bdfeb35b855729777df1e2879aa27130e0
Madhan M <54719274+CaptMadhan@users.noreply.github.com>
2020-12-26 20:15:32 +0530

    26th Dec final Update

--- sql_check.py
+++ sql_check.py
@@ -5,14 +5,10 @@ data_base = base.connect("demo1.db")
 
 # create a cursor
 cursor = data_base.cursor()
-cursor.execute("SELECT cust_id_g FROM customerID_generator where row =1")
-x = cursor.fetchall()
-cust_id_ =x[0][0]+1
-print(cust_id_)
-cursor.execute("UPDATE customerID_generator SET cust_id_g = :cust_id_d WHERE row=1;",{
-    'cust_id_d':x[0][0]+1
-    }
-    )
+cursor.execute("SELECT * from customer;")
+x =cursor.fetchall()
+print(x)
+
     
 
 data_base.close()
